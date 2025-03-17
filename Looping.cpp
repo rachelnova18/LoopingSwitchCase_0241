81dbcd7b9704d082a3bd46f7a4b710b682163183
rachelnova18 <rachelnv26@gmail.com>
2025-03-17 21:25:50 +0700

    membuat perulangan dengan for

--- Looping.cpp
+++ Looping.cpp
@@ -8,3 +8,14 @@ int main()
     int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
+
+    // Perulangan dengan for
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
