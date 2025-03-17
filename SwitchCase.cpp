4bf0ad7cd6e3992413b4b14a610b94e31c6ca6dc
rachelnova18 <rachelnv26@gmail.com>
2025-03-17 21:35:54 +0700

    Membuat menu interaktif dengan perulangan do-while dan switch-case

--- SwitchCase.cpp
+++ SwitchCase.cpp
@@ -9,3 +9,41 @@ float luasLingkaran(float a) {
     return 3.14 * a * a;
 }
 
+int main() {
+    int pilihan;
+    float panjang, lebar;
+    float jariJari;
+
+    do {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                cout << "Hitung luas persegi panjang" << endl;
+                cout << "Masukkan panjang = ";
+                cin >> panjang;
+                cout << "Masukkan lebar = ";
+                cin >> lebar;
+                cout << "Luas persegi panjang = " << luasPersegiPanjang(panjang, lebar) << endl;
+                break;
+            case 2:
+                cout << "Hitung luas lingkaran" << endl;
+                cout << "Masukkan jari-jari = ";
+                cin >> jariJari;
+                cout << "Luas lingkaran = " << luasLingkaran(jariJari) << endl;
+                break;
+            case 3:
+                break;
+            default:
+                cout << "Pilihan salah, pilihan harus di antara 1-3." << endl;
+        }
+    } while (pilihan != 3);
+
+    return 0;
+}
